3f011249c471d1793a387073acc5b2750ecc508a
Eric Wen <wei-yuan.wen@appfolio.com>
2018-10-28 14:41:52 -0700

    Refactor App to support react-navigation

--- App.js
+++ App.js
@@ -1,50 +1,19 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Header, List, ListItem } from 'react-native-elements';
-import CustomListItem from './src/CustomListItem'
+import { createStackNavigator } from 'react-navigation';
+
+import HomeScreen from './src/HomeScreen';
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
 
 export default class App extends Component {
   render() {
-    return (
-      <View style={ styles.container }>
-        <Header
-          backgroundColor='#D94745' 
-          centerComponent={{ text: 'Minimo', style: { color: '#fff', fontSize: 20 } }}
-        />
-        <View>
-          {
-            list.map((item, i) => (
-              <ListItem
-                key={i}
-                title={ <CustomListItem category={ item.title } /> }
-                leftIcon={{ name: item.icon }}
-              />
-            ))
-          }
-        </View>
-      </View>
-    );
+    return <RootStack />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ff9797'
-  }
-});
-
-const list = [
-  {
-    title: 'Food',
-    icon: 'home'
-  },
-  {
-    title: 'House',
-    icon: 'home'
-  },
-  {
-    title: 'Transportation',
-    icon: 'home'
-  }
-]
