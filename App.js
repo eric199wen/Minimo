0d6283a76766b282857d4e6ae18e41bdc3d1c1dc
Eric Wen <wei-yuan.wen@appfolio.com>
2018-10-31 08:10:44 -0700

    Add AddExpense page

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import { createStackNavigator } from 'react-navigation';
 import HomeScreen from './src/HomeScreen';
 import BudgetScreen from './src/BudgetScreen';
 import EditBudget from './src/EditBudget';
+import AddExpense from './src/AddExpense';
 
 const RootStack = createStackNavigator(
   {
     Home: HomeScreen,
     Budget: BudgetScreen,
-    EditBudget: EditBudget
+    EditBudget: EditBudget,
+    AddExpense: AddExpense
   },
   {
     initialRouteName: 'Home',
